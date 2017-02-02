c07ddcd8e38e17edcf8834f9af6809d8dae57998
Faras Momin <famomin@utexas.edu>
2017-02-02 13:52:30 -0600

    Added data in the table for Bamazon App

---
+++ bamazonSeeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO products
+(product_name, department_name, price_to_consumer, stock_quantity)
+VALUES 
+('Amazon Fire TV 4K', 'Media Streaming Devices', 89.99, 1000),
+('Apple MacBook Pro Retina Display 15in.', 'Laptop Computers', 1889.99, 25),
+('Apple iPhone 7 Plus 128gb (Jet Black)', 'Mobile/Cell Phone', 849.99, 100),
+('Sony Bravia 65in 4K Ultra HD XBR-65X930D', 'Electronics', 1798.00, 10),
+('Amazon Fire TV 4K', 'Media Streaming Devices', 89.99, 1000),
+('Cards Against Humanity', 'Card Games', 25.99, 225),
+('Creed Aventus 4 oz.', 'Personal Care Fragnance', 425.00, 15),
+('RITC 30 oz. Tumbler', 'Drinkware', 24.99, 120),
+('TI Nspire CX CAS Graphing Calculatior', 'Graphing Calculators', 139.99, 75),
+('KitchenAid Professional 6000 HD', 'Kitchen Appliances', 265.99, 2),
+('Altra Furniture Pursuit U-Shaped Desk', 'Home/Office Furniture', 665.05, 1);
