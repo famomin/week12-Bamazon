d8daf8763f8a5f2d32514a1b36729f6473c8cb62
Faras Momin <famomin@utexas.edu>
2017-02-04 00:22:57 -0600

    app is providing subtotal, purchased items, and gives error if not enough stock available. getting node:1968 error

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,45 +15,70 @@ var connection = mysql.createConnection ({
 
 connection.connect (function (err){
 	if (err) throw err;
-	console.log("connected as id " + connection.threadId);
+	shop()
 });
 
-connection.query("Select * FROM products", function (err, res) {
-	if (err) throw err;
 
-	for (var i = 0; i < res.length; i++) {
-		console.log("Product ID: " + res[i].item_id + " | Product Name: " + res[i].product_name + " | Department: " + res[i].department_name +  " | Price per unit: " + res[i].price_to_consumer + " | Quantity Available: " + res[i].stock_quantity);
-	}
-});
+function shop (){ 
 
-connection.end();
+	connection.query("Select * FROM products", function (err, res) {
+		if (err) throw err;
 
-//asking people what item they want to buy
-inquirer.prompt([
-
-	//asking for what item they want to buy
-	{
-		type: "input",
-		name: "item",
-		message: "Please enter Product ID for the item you want to purchase."
-	},
-
-	{
-		type: "input",
-		name: "quantity",
-		message: "Please enter quantity you want you purchase."
-
-	}
-]).then(function (user){
-
-	//check if item number exists
-	for (var i = 1; i <= res.length; i++){
-		if(user.item ===res[i].item_id) {
-			
-		}	
-	}
+		for (var i = 0; i < res.length; i++) {
+			console.log("Product ID: " + res[i].item_id + " | Product Name: " + res[i].product_name + " | Department: " + res[i].department_name +  " | Price per unit: " + res[i].price_to_consumer + " | Quantity Available: " + res[i].stock_quantity);
+		}
 	
-});
+
+		//asking people what item they want to buy
+		inquirer.prompt([
+
+			//asking for what item they want to buy
+			{
+				type: "input",
+				name: "item",
+				message: "Please enter Product ID for the item you want to purchase."
+			},
+
+			{
+				type: "input",
+				name: "quantity",
+				message: "Please enter quantity you want you purchase."
+
+			}
+		]).then(function (user){
+
+			//check if item number exists
+			//starting with for loop to loop thru all items ids
+			for (var i = 1; i <= res.length; i++) {
+
+				//checking if user input item id matches database or not
+				if(parseInt(user.item) ===res[i].item_id) {
+
+					//if items id matches, then check if enough quantity is available or not.
+					if(res[i].stock_quantity >= parseInt(user.quantity)) {
+						console.log("You bought " + parseInt(user.quantity) + " " + res[i].product_name);
+						console.log("Your total for this purchase is $" + parseInt(user.quantity)*res[i].price_to_consumer)
+					}
+
+					//if not enough quanitty then say no enough quantity.
+					else {
+						console.log("Not Enough left in stock.");
+					}
+
+				}
+
+				// //if user inpute item  id does not match then error.
+				// else{
+				// 	console.log("Product Id Invalid")
+				// }	
+			}
+			
+		});
+	});
+
+
+}
+
 
 
 
